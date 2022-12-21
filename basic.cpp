31c159cc152ec9ae1fc4f63e4fcb25000bd8e61d
ZyadKhan <74572665+ZyadKhan05@users.noreply.github.com>
2022-12-20 22:49:40 -0500

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    // Basic Tutorial:
+     std::cout << "Hello World"; // Print Statement
+     return 0; // 0 tells the operating system that the program will terminate correctly
+}
