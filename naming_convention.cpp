0c1ebdf2e4b104f4db76171b57bb4fdebfb3f7a4
ZyadKhan <74572665+ZyadKhan05@users.noreply.github.com>
2022-12-20 22:47:24 -0500

    Create naming_convention.cpp

---
+++ naming_convention.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Naming Conventions
+    int file_size; // Snake Case - lowercase letters to name our variables & constants, if we have multiple words separate them with underscore
+    int FileSize; // Pascal Case
+    int fileSize; // Camel Case
+    int iFileSize; // Hungarian Notation - Use the file type in the front of the variable/constant
+    return 0;
+}
