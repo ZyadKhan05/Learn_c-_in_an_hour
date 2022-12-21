02580f93b344e6cb4f14619a447bbcdcee075e4d
ZyadKhan <74572665+ZyadKhan05@users.noreply.github.com>
2022-12-20 22:38:42 -0500

    Create standard_library.cpp

---
+++ standard_library.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+// Working with the Standard Library
+    // Import <cmath> library on the top of the code
+    double result = floor(1.2) ; // Floor function
+    double result = pow(2, 3); // Power
+    cout << result;
+    return 0;
+ }
