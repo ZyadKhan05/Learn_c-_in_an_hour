d80a72696e44c73486845f92fc141e35f0d7849e
ZyadKhan <74572665+ZyadKhan05@users.noreply.github.com>
2022-12-20 22:36:12 -0500

    Create comments.cpp

---
+++ comments.cpp
@@ -0,0 +1,7 @@
+    // Comments
+    // Comment  - First type of comment: can be added to the side of a line, but is more conventional to put on top
+    /*
+     Comment - Second type of comment
+     */
+
+    // Do not over use comments - only use to explain whys and hows
