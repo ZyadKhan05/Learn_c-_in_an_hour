86c13f62b129102016c79bfa9a120a65f67d6075
ZyadKhan <74572665+ZyadKhan05@users.noreply.github.com>
2022-12-20 22:49:01 -0500

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    // Variables tutorial
+    int file_size = 0 ; // Use meaningful for variable names - No abbreviated names and make it something that someone would know what it is
+    int counter = 0;
+    double sales = 9.99;
+    std::cout << file_size; // Statement
+    return 0;
+}
